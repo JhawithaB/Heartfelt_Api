1f5c40eec89cf3ee3e1855f4eaa6d9e17d417ea8
JhawithaB <beltranjhanna8@gmail.com>
2026-03-08 17:02:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import random
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -25,4 +26,4 @@ def random_question():
     return jsonify(random.choice(trivia))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
